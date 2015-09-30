4c298ceb38274affac75f19cea9edbb4fdfe4d81
wellington <wellpinho@outlook.com>
2015-09-30 13:25:03 -0300

    Create exercicio1.py

---
+++ exercicio1.py
@@ -0,0 +1,4 @@
+"""Sabemos que o input pega dados inseridos pelo usuário.
+este exercicio é exclusivamente para uso do input.
+com base nisso crie uma mini carta de amor para sua namorada, onde na carta contenha o nome do rapaz, e o nome da moça.
+a cartinha deve ter os nomes espalhados por todo o texto e que contenha no mínimo 2 repetições dos nomes de ambos."""
