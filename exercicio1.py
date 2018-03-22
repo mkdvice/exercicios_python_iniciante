cc656d9d40fe054c36c4e7a781879c7f9b42b48e
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-03-22 11:09:05 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -3,11 +3,21 @@ este exercicio é exclusivamente para uso do input.
 com base nisso crie uma mini carta de amor para sua namorada, onde na carta contenha o nome do rapaz, e o nome da moça.
 a cartinha deve ter os nomes espalhados por todo o texto e que contenha no mínimo 2 repetições dos nomes de ambos."""
 
+#Carta para Namorada
+
+Print('''Para Ann
+         
+         Olá, Ann. Tudo bem? Estou em Madrid agora. A cidade é linda, há varias catedrais.
+       Logo, logo você estara aqui comigo.
+       
+       Saudades,
+       Axl''') # Para escrever textos com várias linhas é utlizado três aspas simples
+
 # Exemplo de input
 
 print(input('Digite algo: '))
 
-# Exemplo de input de número
+# Exemplo de input de número inteiro
 
 num = int(input('Digite um número: '))
 
