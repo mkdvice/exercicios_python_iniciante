b135b8d895f41ff034d33b4e1fd9478513d10582
Ezequiel Alexandre <ezequielaraujo1997@gmail.com>
2018-03-21 22:46:37 -0300

    ExemplosDeInput
    Foi dado exemplos de "input" para serem utilizados em aplicações futuras.

--- exercicio1.py
+++ exercicio1.py
@@ -2,3 +2,15 @@
 este exercicio é exclusivamente para uso do input.
 com base nisso crie uma mini carta de amor para sua namorada, onde na carta contenha o nome do rapaz, e o nome da moça.
 a cartinha deve ter os nomes espalhados por todo o texto e que contenha no mínimo 2 repetições dos nomes de ambos."""
+
+# Exemplo de input
+
+print(input('Digite algo: '))
+
+# Exemplo de input de número
+
+num = int(input('Digite um número: '))
+
+# Exemplo de input com string
+
+nome = str(input('Digite seu nome: '))
